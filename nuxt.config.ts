c7a974816be8ca1e383f118d1c93c2614e30c0f0
Mapsqc <168664591+Mapsqc@users.noreply.github.com>
2026-03-26 16:02:32 -0400

    fix: exclude /vad/ from Supabase auth redirect
    Static WASM/ONNX files were getting 302 redirected to /login

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
     redirectOptions: {
       login: '/login',
       callback: '/confirm',
-      exclude: ['/login', '/signup'],
+      exclude: ['/login', '/signup', '/vad/*'],
     },
   },
   vite: {
